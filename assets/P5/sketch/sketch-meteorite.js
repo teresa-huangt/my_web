39405a5fbf8476576b690fe6311259e749678f74
zcc <listenzcc@mail.bnu.edu.cn>
2019-03-10 00:31:44 +0800

    modify, index.html - mainpage, meteorite

--- assets/P5/sketch/sketch-meteorite.js
+++ assets/P5/sketch/sketch-meteorite.js
@@ -5,7 +5,7 @@ let myMap;
 const mappa = new Mappa('Leaflet');
 const options = {
 	lat: 0,
-	lng: 150,
+	lng: 0,
 	zoom: 1.8,
 	style: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
 }
@@ -13,10 +13,12 @@ const options = {
 // Define other parameters
 var data;
 let allCoordinates = [];
+let hist = []
+let maxhist = 0
 
 let size;
 let maxmass = 0;
-let num = 50;
+let num = 500;
 let maxyear = 0;
 let minyear = 0;
 
@@ -53,9 +55,11 @@ function setup() {
   print(level0);
 
   cal_yearRange();
+  cal_hist();
   cal_massMax();
-  show_Introduction();
-  myMap.onChange(drawPoints);
+  //show_Introduction();
+  //myMap.onChange(drawPoints);
+  myMap.onChange(draw);
 
   // Create GUI
   //get_yearRange();
@@ -63,6 +67,23 @@ function setup() {
   // yearSlider.position (20,20);
   // cSlider = createSlider(0,255,100);
   // cSlider.position (20,50);
+}
+
+function cal_hist(){
+  for (var k = 0; k < num; k++) {
+    year = int(level0[k].year)
+    for (var y = year-5; y < year+5; y++) {
+      if (hist[y] == undefined){
+        hist[y] = 1
+      } else {
+        hist[y] += 1
+      }
+      if (hist[y] > maxhist) {
+        maxhist = hist[y]
+      }
+    }
+  }
+  print('maxhist is %s', maxhist)
 }
 
 
@@ -75,6 +96,7 @@ function cal_yearRange(){
       maxyear = level0[k].year;
     }
   }
+  maxyear = int(maxyear)
   print('yearMax is %s', maxyear);
 
   // minyear
@@ -83,6 +105,9 @@ function cal_yearRange(){
     if (int(minyear) > int(level0[l].year)){
       minyear = level0[l].year;
     }
+  }
+  if (int(minyear) < 1793){
+    minyear = 1793
   }
   print('yearMin is %s', minyear);
 }
@@ -100,10 +125,11 @@ function cal_massMax() {
 
 // 
 function show_Introduction(){
-fill(142, 53, 74);
-text("Move mouse to change time range", 0, height-60)
+  fill(200)
+  textSize(20)
+  text("Move your mouse.", 10, height-60)
+  textSize(14)
 }
-
 
 
 function draw() {
@@ -111,12 +137,6 @@ function draw() {
     cal_mouseyear()
     drawPoints()
     draw_scroll()
-    fill(255, 255, 255)
-    if (mouseY < (height-10) ) {
-      text(str(mouseyear-mousegap)+' - '+str(mouseyear+mousegap), mouseX+5, mouseY+10)
-    } else {
-      text(str(mouseyear-mousegap)+' - '+str(mouseyear+mousegap), mouseX+5, mouseY-10)
-    }
   }
 }
 
@@ -125,32 +145,53 @@ function is_in_scroll() {
   if (mouseX > width) return false
   if (mouseX < 0) return false
   if (mouseY > height) return false
-  if (mouseY < height-50) return false
+  if (mouseY < 0) return false
   return true
 }
 
 // Map year range to the rectangle range
 function cal_mouseyear(){
   mouseyear = int(map(mouseX, 0, width, int(minyear), int(maxyear)))
-  mousegap = int(map(mouseY, height-50, height, 10, 100))
+  mousegap = int(map(mouseY ** 3, 0, height ** 3, 10, 100))
 }
 
 // Draw meteorites using ellipse
 function drawPoints(){
   clear();
+  lx = -1
+  ly = -1
+  a = 200
   for(var i = 0; i < num; i++){
     let point = level0[i]
     if (!is_goodyear(point.year, mouseyear-mousegap, mouseyear+mousegap)){
       continue
     }
+    if (point.reclat == undefined) {
+      continue
+    }
+    if (lx == -1) {
+      lx = point.reclat
+      ly = point.reclong
+    } else {
+      x = point.reclat
+      y = point.reclong
+      pos1 = myMap.latLngToPixel(lx, ly)
+      pos2 = myMap.latLngToPixel(x, y)
+      stroke(200, a)
+      if (a > 10) {
+        a = a - 10
+      }
+      line(pos1.x, pos1.y, pos2.x, pos2.y)
+      lx = x
+      ly = y
+    }
     draw_fallpoint(point)
   } 
-  draw_scroll()
 }
 
 function draw_fallpoint(point){
   let pos = myMap.latLngToPixel(point.reclat, point.reclong)
-  let normalizeSize = map(log(point.mass), 0, log(maxmass), 0, 50);
+  let normalizeSize = map(log(point.mass), 0, log(maxmass), 0, 30);
   prepare_draw_fallpoint()
   ellipse(pos.x, pos.y, normalizeSize, normalizeSize);
   fill(134, 226, 213)
@@ -177,8 +218,32 @@ function draw_scroll(){
   noStroke()
   fill(142, 53, 74)
   rect(0, height-50, width, 50)
-  fill(241, 169, 160,90)
+  
+  fill(241, 169, 160, 90)
   rect(mouseX-mousegap/2, height-50, mousegap,50)
+
+  fill(200)
+  text(str(mouseyear-mousegap)+' - '+str(mouseyear+mousegap), mouseX+5, height-60)
+  show_Introduction()
+  draw_hist()
+}
+
+function draw_hist(){
+  stroke(255, 255, 255)
+  fill(255, 255, 255)
+  lx = 0
+  ly = height
+  for (var j = minyear; j < maxyear; j++){
+    h = hist[j]
+    if (h == undefined){
+      continue
+    }
+    x = map(j, minyear, maxyear, 0, width)
+    y = map(h, 0, maxhist, 0, 50)
+    line(lx, height-ly, x, height-y)
+    lx = x
+    ly = y
+  }
 }
 
 
